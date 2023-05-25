3438b20240dda5850f6030dc7c9223e1b3283392
msin <msin@MSI>
2023-05-25 12:03:47 +0700

    nop bai tuan 4

--- demo/src/main/java/nguyentanhoang/demo/services/CategoryService.java
+++ demo/src/main/java/nguyentanhoang/demo/services/CategoryService.java
@@ -9,11 +9,32 @@ import java.util.List;
 
 @Service
 public class CategoryService {
-    @Autowired
-    private ICategoryRepository categoryRepository;
-    public List<Category> getAllCategories(){return categoryRepository.findAll(); }
+    private final ICategoryRepository categoryRepository;
 
-    public Category getCategoryById(Long id){return categoryRepository.findById(id).orElse(null);}
-    public Category saveCategory(Category category) {return categoryRepository.save(category);}
-    public void deleteCategory(Long id) {categoryRepository.deleteById(id);}
+    @Autowired
+    public CategoryService(ICategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public void addCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category ID: " + id));
+    }
+
+    public void updateCategory(Category category) {
+        categoryRepository.save(category);
+    }
 }
+
